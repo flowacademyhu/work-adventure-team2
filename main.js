f5aa2bdfef389cb2e31a36ae3080dba8e7a4cb71
Dani <farkika39@gmail.com>
2020-05-20 18:54:22 +0200

    add text

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const location = {
   y: 0
 };
 console.log('Your options:\n 1: Southwest;\n 2: South;\n 3: Southeast;\n 4: West;\n 6: East;\n 7: Northwest;\n 8: North;\n 9: Northeast.');
-
 console.log('Fésűs Éva: A büszke tölgyfa');
+console.log('Volt egyszer a köröskörül erdőben egy sudár, fiatal tölgyfa. Kék ég felé nyújtózkodott, lombjain át arany napfényt szitálgatott, és erős gyökerével a föld minden erejét magába szívta. Ő volt a legszebb az erdőn.');
 
 while (location.x !== 4 || location.y !== 4) {
   const dir = input.inputFromNumPad();
