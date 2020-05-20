ae55c78ba84981d8e30dc38fefb340609846b5d1
Redturtle <izabella.tanacs@yahoo.com>
2020-05-20 19:08:11 +0200

    input.js and output.js are modified

--- output.js
+++ output.js
@@ -1,1 +1,8 @@
-console.log('Your options:\n 1: Southwest;\n 2: South;\n 3: Southeast;\n 4: West;\n 6: East;\n 7: Northwest;\n 8: North;\n 9: Northeast.');
+const print = (location, map) => {
+  console.log(map.map[location.y][location.x].label + ' mezőben állsz most.\n');
+  console.log(map.map[location.y][location.x].text);
+};
+
+module.exports = {
+  print
+};
