67ebdc2bcf5d2df131969d32cbba33738bf54fad
Dani <farkika39@gmail.com>
2020-05-20 20:53:42 +0200

    finnished work

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const location = {
   x: 0,
   y: 0
 };
+output.mapPrinting(location, map);
+map.mapOut[location.y][location.x] = ' ';
 console.log('Your options:\n 1: Southwest;\n 2: South;\n 3: Southeast;\n 4: West;\n 6: East;\n 7: Northwest;\n 8: North;\n 9: Northeast.');
 console.log('Fésűs Éva: A büszke tölgyfa');
 console.log('Volt egyszer a köröskörül erdőben egy sudár, fiatal tölgyfa. Kék ég felé nyújtózkodott, lombjain át arany napfényt szitálgatott, és erős gyökerével a föld minden erejét magába szívta. Ő volt a legszebb az erdőn.');
@@ -14,8 +16,8 @@ console.log('Volt egyszer a köröskörül erdőben egy sudár, fiatal tölgyfa.
 while (location.x !== 4 || location.y !== 4) {
   const dir = input.inputFromNumPad();
   navigation.move(location, dir);
-  // output.print(location, map);
   output.mapPrinting(location, map);
+  output.print(location, map);
   map.mapOut[location.y][location.x] = ' ';
 }
 console.log('Vége!');
--- output.js
+++ output.js
@@ -1,12 +1,12 @@
 const table = require('table');
 
 const print = (location, map) => {
-  console.log(map.map[location.y][location.x].label + ' mezőben állsz most.\n');
+  // console.log(map.map[location.y][location.x].label + ' mezőben állsz most.\n');
   console.log(map.map[location.y][location.x].text);
 };
 
 const mapPrinting = (location, map) => {
-  
+
   process.stdout.write('\033[2J');
   map.mapOut[location.y][location.x] = 'x';
   console.log(table.table(map.mapOut));
