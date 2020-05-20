83df115156f44c821902686d5d0580666955ca93
Dani <farkika39@gmail.com>
2020-05-20 18:19:04 +0200

    adding options

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const navigation = require('./navigation');
 const map = require('./map');
 
 const location = {
-  x: 0,
+  x: -1,
   y: 0
 };
+console.log('Your options:\n 1: Southwest;\n 2: South;\n 3: Southeast;\n 4: West;\n 6: East;\n 7: Northwest;\n 8: North;\n 9: Northeast.');
+
 console.log('Fésűs Éva: A büszke tölgyfa');
 
 while (location.x !== 4 || location.y !== 4) {
