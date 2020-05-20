067eb7d24685072a64a26927c2527dc7b6dd74cd
Redturtle <izabella.tanacs@yahoo.com>
2020-05-20 20:27:47 +0200

    new table is added

--- output.js
+++ output.js
@@ -1,8 +1,15 @@
+const table = require('table');
+
 const print = (location, map) => {
   console.log(map.map[location.y][location.x].label + ' mezőben állsz most.\n');
   console.log(map.map[location.y][location.x].text);
 };
 
+const mapPrinting = (location) => {
+  map.mapOut[location.y][location.x] = 'x';
+};
+
 module.exports = {
-  print
+  print,
+  mapPrinting
 };
