8a8e2d1143bde16d751014d8c93d0fcd7d5e8667
Dani <farkika39@gmail.com>
2020-05-20 15:15:28 +0200

    adding main + package

---
+++ main.js
@@ -0,0 +1,14 @@
+const input = require('./input');
+const navigation = require('./navigation');
+// const output = require('./output');
+// const map = require('./map');
+
+/*
+const location = {
+  x: 0,
+  y: 0
+};
+const dir = input.inputFromNumPad();
+navigation.move(location, dir);
+console.log(location);
+*/
