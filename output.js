9856c6e8a164798dfd4c56992f3d301cdd2dae03
Dani <farkika39@gmail.com>
2020-05-21 11:12:27 +0200

    write the direction every time

--- output.js
+++ output.js
@@ -3,6 +3,7 @@ const axel = require('axel');
 
 const print = (location, map) => {
   // console.log(map.map[location.y][location.x].label + ' mezőben állsz most.\n');
+  console.log('Your options:\n 1: Southwest;\n 2: South;\n 3: Southeast;\n 4: West;\n 6: East;\n 7: Northwest;\n 8: North;\n 9: Northeast.');
   console.log(map.map[location.y][location.x].text);
 };
 
